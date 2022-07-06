71157f78acffc0f9fd9a5cbfbceffb4e347b473c
Ingy döt Net <ingy@ingy.net>
2022-07-06 15:11:25 -0700

    Add skeleton for Python

---
+++ python/ReadMe.md
@@ -0,0 +1,31 @@
+yamlscript
+==========
+
+Programming in YAML
+
+## Synopsis
+```
+#!/usr/bin/env yamlscript.py
+- $greetee: world
+- +say: Hello, $greetee!
+```
+
+## Status
+
+This module is *very* **ALPHA**.
+
+YAMLScript is still being defined.
+
+Use with caution for now.
+
+## Description
+
+YAMLScript is a programming language encoded in YAML.
+
+## License & Copyright
+
+This project is licensed under the terms of the `MIT` license.
+See [LICENSE](https://github.com/yaml/pyyaml-future/blob/main/LICENSE) for more
+details.
+
+Copyright 2022 Ingy döt Net <ingy@ingy.net>
---
+++ python/setup.py
@@ -0,0 +1,48 @@
+version = '0.0.1'
+
+from setuptools import setup
+import pathlib
+
+root = pathlib.Path(__file__).parent.resolve()
+
+long_description = \
+  (root / '.long_description.md') \
+  .read_text(encoding='utf-8')
+
+setup(
+  name = 'yamlscript',
+  version = version,
+  description = 'Program in YAML',
+  license = 'MIT',
+  url = 'https://github.com/ingydotnet/yamlscript',
+
+  author = 'Ingy döt Net',
+  author_email = 'ingy@ingy.net',
+
+  packages = ['yamlscript'],
+  package_dir = {'': 'lib'},
+
+  python_requires = '>=3.6, <4',
+  install_requires = [
+    'pyyaml',
+  ],
+  setup_requires = [
+    'wheel',
+  ],
+
+  keywords = ['yaml', 'language'],
+  classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3 :: Only',
+  ],
+
+  long_description = long_description,
+  long_description_content_type = 'text/markdown',
+)
