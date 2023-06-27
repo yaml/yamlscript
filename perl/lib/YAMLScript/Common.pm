512707f8d9d8ff3057240d9abb1eddb10980f5f9
Ingy döt Net <ingy@ingy.net>
2023-06-27 17:24:58 -0400

    Fix YAMLScript/Lingy bootstrap code

--- perl/lib/YAMLScript/Common.pm
+++ perl/lib/YAMLScript/Common.pm
@@ -2,12 +2,16 @@ use strict; use warnings;
 package YAMLScript::Common;
 
 use Exporter 'import';
-use Lingy::Common;
+use Lingy::Common();
 
 BEGIN {
     package Lingy::Common;
     no warnings 'redefine';
-    sub RT() { 'YAMLScript::RT' }
+    sub RT { 'YAMLScript::RT' }
+}
+
+BEGIN {
+    Lingy::Common->import;
 }
 
 our @EXPORT = @Lingy::Common::EXPORT;
