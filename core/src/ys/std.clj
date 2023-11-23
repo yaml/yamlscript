4978859615854014802221916f2826afe06deeff
Ingy döt Net <ingy@ingy.net>
2023-11-23 16:37:37 -0500

    core: Add new functions to ys.std

--- core/src/ys/std.clj
+++ core/src/ys/std.clj
@@ -5,14 +5,55 @@
 
 (ns ys.std
   (:require
-   [yamlscript.debug :refer [www]]))
+   [yamlscript.debug :refer [www]]
+   [clojure.pprint :as pp]
+   [clojure.string :as str])
+  (:refer-clojure :exclude [print]))
 
-(defn say [& more]
-  (apply clojure.core/println more))
+(defn B [x] boolean x)
+
+(defn F [x] (parse-double x))
+
+(defn I [x] (parse-long x))
+
+(defn M
+  ([] {})
+  ([x] (apply hash-map x))
+  ([k v & xs] (apply hash-map k v xs)))
+
+(defn S [& xs] (apply str xs))
+
+(defn => [this] this)
+
+(defn err [& xs]
+  (binding [*out* *err*]
+    (apply clojure.core/print xs)
+    (flush)))
+
+(defn out [& xs]
+  (apply clojure.core/print xs)
+  (flush))
+
+(defn pretty [o]
+  (str/trim-newline
+    (with-out-str
+      (pp/pprint o))))
+
+(defn print [o]
+  (clojure.core/print o)
+  (flush))
 
 (defn rng [x y]
   (if (> y x)
     (range x (inc y))
     (range x (dec y) -1)))
+
+(defn say [& xs]
+  (apply clojure.core/println xs))
+
+(defn warn [& xs]
+  (binding [*out* *err*]
+    (apply clojure.core/println xs)
+    (flush)))
 
 (comment)
