a8cb0626b2dec14de5e2e6bc9365b7d1088eee86
Ingy döt Net <ingy@ingy.net>
2023-12-05 21:27:34 -0800

    Tue Dec  5 09:27:34 PM PST 2023

---
+++ assets/main.js
@@ -0,0 +1,16 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other entry modules.
+(() => {
+
+})();
+
+// This entry need to be wrapped in an IIFE because it need to be in strict mode.
+(() => {
+"use strict";
+// extracted by mini-css-extract-plugin
+
+})();
+
+/******/ })()
+;
