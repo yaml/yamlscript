9b86d3a75a16ff42de54d125dc9499f1db90b4a0
Ingy döt Net <ingy@ingy.net>
2023-12-23 06:39:31 -0800

    perl: Fix load to return $resp->{data}

--- perl/lib/YAMLScript/FFI.pm
+++ perl/lib/YAMLScript/FFI.pm
@@ -19,11 +19,25 @@ $ffi->function( graal_create_isolate => [qw( opaque opaque* opaque* )] => 'int' 
     and die 'Could not setup evaluation';
 
 $ffi->attach(
-    [ load_ys_to_json => 'load' ] => [qw( sint64 string )] => 'string' => sub {
+    [ load_ys_to_json => '_load' ] => [qw( sint64 string )] => 'string' => sub {
         my ( $xsub, $ys ) = @_;
         my $data = $xsub->( $thread, $ys );
         JSON::decode_json($data);
     },
 );
+
+sub load {
+    my ($ys_code) = @_;
+    my $resp = _load($ys_code);
+    if (my $data = $resp->{data}) {
+        return $data;
+    }
+    elsif (my $error = $resp->{error}{cause}) {
+        die "libyamlscript: $error";
+    }
+    else {
+        die "YAMLScript::FFI unkown error";
+    }
+}
 
 1;
