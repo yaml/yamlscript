5b88f9ae47ac54d4af63407fe043649dd7165193
Ethiraric <ethiraric@gmail.com>
2024-01-01 22:01:36 +0100

    rust: Harmonize interface with other languages
    Have a single `load` function that calls `libyamlscript.so`, parses the
    returned value with `serde_json` and returns the target object directly.
    Previous functions have been deleted as they are outdated.
    
    This commit also includes 2 very basic tests that just test that we can
    retrieve a json object from libyamlscript both through
    `serde_json::Value` and a custom structure implementing `Deserialize`.

--- rust/examples/eval_ys_to_json.rs
+++ rust/examples/eval_ys_to_json.rs
@@ -1,5 +1,5 @@
 fn main() {
     let input = std::io::read_to_string(std::io::stdin()).unwrap();
-    let output = yamlscript::load(&input).unwrap();
+    let output = yamlscript::load::<serde_json::Value>(&input).unwrap();
     println!("{output}");
 }
