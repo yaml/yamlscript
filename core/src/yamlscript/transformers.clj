2d2e8ac0c6df46840e40a5e692fcf54221f3c82d
Ingy döt Net <ingy@ingy.net>
2024-02-12 16:34:20 -0800

    core: Finish `require` transformations

--- core/src/yamlscript/transformers.clj
+++ core/src/yamlscript/transformers.clj
@@ -9,40 +9,82 @@
 
 (def Q {:Sym 'quote})
 
-(defn transform_cond [key val]
-  (when-lets [forms (:forms val)
+;;-----------------------------------------------------------------------------
+;; cond
+;;-----------------------------------------------------------------------------
+
+(defn transform_cond [lhs rhs]
+  (when-lets [forms (:forms rhs)
               len (count forms)
               _ (>= len 2)
               last-key-pos (- len 2)
               last-key (nth forms last-key-pos)
               _ (= '=> (:Sym last-key))
-              val (update-in val [:forms last-key-pos] (fn [_] (Key "else")))]
-    [key val]))
+              rhs (update-in rhs [:forms last-key-pos] (fn [_] (Key "else")))]
+    [lhs rhs]))
 
-(defn transform_require [key val]
-  (if-lets [_ (:Sym key)
-            _ (:Spc val)]
-    [key (Lst [Q val])]
-    ,
-    (if-lets [sym (get-in key [0])
+;;-----------------------------------------------------------------------------
+;; require
+;;-----------------------------------------------------------------------------
+
+(def AS (Key "as"))
+(def REFER (Key "refer"))
+
+(defn require-spc-lhs? [lhs]
+  (when-lets [sym (get-in lhs [0])
               _ (:Sym sym)
-              spc (nth key 1)
+              spc (nth lhs 1)
               _ (:Spc spc)
-              _ (= 2 (count key))
-              _ (nil? val)]
-      [sym (Lst [Q spc])]
-      ,
-      (if-lets [sym (get-in key [0])
-                _ (:Sym sym)
-                spc (nth key 1)
-                _ (:Spc spc)
-                _ (= 2 (count key))
-                _ (= '=> (get-in val [0 :Sym]))
-                alias (nth val 1)
-                _ (:Sym alias)]
-        [sym (Lst [Q (Vec [spc (Key "as") alias])])]
-        ,
-        (throw (Exception. "Invalid 'require' form"))))))
+              _ (= 2 (count lhs))]
+    [sym spc]))
+
+(defn require-args [rhs]
+  (let [args []
+        rhs (if (vector? rhs) rhs [rhs])
+        [args rhs] (if-lets [_ (= '=> (get-in rhs [0 :Sym]))
+                             alias (nth rhs 1)
+                             _ (:Sym alias)]
+                     [(conj args AS alias) (drop 2 rhs)]
+                     [args rhs])
+        args (if (seq rhs)
+               (if (every? :Sym rhs)
+                 (conj args REFER (Vec rhs))
+                 (throw (Exception. "Invalid 'require' arguments")))
+               args)]
+    args))
+
+(defn require-pairs [pairs]
+  (reduce
+    (fn [acc [spc rhs]]
+      (or (:Spc spc)
+        (throw (Exception. "Invalid 'require' pairs")))
+      (let [args (if (nil? rhs)
+                   (Lst [Q spc])
+                   (Lst [Q (Vec (concat [spc] (require-args rhs)))]))]
+        (conj acc args)))
+    []
+    (partition 2 pairs)))
+
+(defn transform_require [lhs rhs]
+  (or
+    (when-lets [_ (:Sym lhs)
+                _ (:Spc rhs)]
+      [lhs (Lst [Q rhs])])
+    ,
+    (when-lets [[sym spc] (require-spc-lhs? lhs)
+                _ (nil? rhs)]
+      [sym (Lst [Q spc])])
+    ,
+    (when-lets [[sym spc] (require-spc-lhs? lhs)
+                args (require-args rhs)]
+      [sym (Lst [Q (Vec (concat [spc] args))])])
+    ,
+    (when-lets [_ (:Sym lhs)
+                pairs (:pairs rhs)
+                args (require-pairs pairs)]
+      [lhs args])
+    ,
+    (throw (Exception. "Invalid 'require' form"))))
 
 (comment
   www)
