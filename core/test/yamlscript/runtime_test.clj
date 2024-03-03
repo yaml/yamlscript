4d635f1d3bfc3a7c15675feab21a7e90f6b71718
Ingy döt Net <ingy@ingy.net>
2024-03-03 12:55:26 -0800

    core: Add runtime tests

---
+++ core/test/yamlscript/runtime_test.clj
@@ -0,0 +1,24 @@
+; Copyright 2023-2024 Ingy dot Net
+; This code is licensed under MIT license (See License for details)
+
+(ns yamlscript.runtime-test
+  (:use yamlscript.debug)
+  (:require
+   [clojure.edn :as edn]
+   [yamlscript.compiler :as compiler]
+   [yamlscript.runtime :as runtime]
+   [yamltest.core :as test]))
+
+(test/load-yaml-test-files
+  ["test/runtime.yaml"]
+  {:pick #(test/has-keys? [:ys :eval] %1)
+   :test (fn [test]
+           (-> test
+             :ys
+             (->> (str "!yamlscript/v0\n"))
+             compiler/compile
+             runtime/eval-string))
+   :want (fn [test]
+           (-> test
+             :eval
+             edn/read-string))})
