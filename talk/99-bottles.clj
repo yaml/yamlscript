d4cd9cd7cca651d3673b2ed677b3ee984b0d3de6
Ingy döt Net <ingy@ingy.net>
2024-03-06 22:59:29 -0800

    talk: OSSNA April 2024

--- talk/99-bottles.clj
+++ talk/99-bottles.clj
@@ -1,5 +1,4 @@
 (declare paragraph bottles)
-
 (defn main
   ([number] (each [n (rng number 1)]
               (say (paragraph n))))
@@ -7,9 +6,12 @@
 
 (defn paragraph [num]
   (str
-    (bottles num) " of Lisp on the wall,\n"
-    (bottles num) " of Lisp.\n(do (take 1 down) (cons it around))\n"
-    (bottles (- num 1)) " of Lisp on the wall." "\n"))
+    (bottles num)
+    " of Lisp on the wall,\n"
+    (bottles num)
+    " of Lisp.\n(do (take 1 down) (cons it around))\n"
+    (bottles (- num 1))
+    " of Lisp on the wall." "\n"))
 
 (defn bottles [n]
   (cond
