28c6c59c62ad9430c98cb08d81586181cac0bf1d
Rayat M Rahman <22646419+riotrah@users.noreply.github.com>
2024-03-10 06:32:05 -0700

    try koffi :shrug:

--- node/index.ts
+++ node/index.ts
@@ -1,5 +1,4 @@
 // Copyright 2023-2024 Ingy dot Net
-// This code is licensed under MIT license (See License for details)
 
 /**
  * TypeScript binding/API for the libyamlscript shared library.
@@ -13,27 +12,22 @@
  * object that the YAMLScript code evaluates to.
  */
 
-import { dlopen, FFIType, suffix, ptr } from "bun:ffi";
-import * as os from "os";
+import { ptr, suffix } from "bun:ffi";
+import koffi from "koffi";
 import * as fs from "fs";
+import * as os from "os";
 import * as path from "path";
 
-// This value is automatically updated by 'make bump'.
-// TODO: ^ this is not actually true yet, make it so.
-//
-// The version number is used to find the correct shared library file.
-// We currently only support binding to an exact version of libyamlscript.
-const yamlscriptVersion = '0.1.41';
+const yamlscriptVersion = "0.1.41";
 
-// Find the libyamlscript shared library file path:
 function findLibyamlscriptPath() {
   // bun:ffi's `suffix` is automatically os-specific.
   const libyamlscriptName = `libyamlscript.${suffix}.${yamlscriptVersion}`;
 
-  const ldLibraryPath = process.env["LD_LIBRARY_PATH"] || '';
-  const ldLibraryPaths = ldLibraryPath.split(':');
-  ldLibraryPaths.push('/usr/local/lib');
-  ldLibraryPaths.push(path.join(os.homedir(), '.local', 'lib'));
+  const ldLibraryPath = process.env["LD_LIBRARY_PATH"] || "";
+  const ldLibraryPaths = ldLibraryPath.split(":");
+  ldLibraryPaths.push("/usr/local/lib");
+  ldLibraryPaths.push(path.join(os.homedir(), ".local", "lib"));
 
   for (const ldPath of ldLibraryPaths) {
     const libyamlscriptPath = path.join(ldPath, libyamlscriptName);
@@ -45,68 +39,110 @@ function findLibyamlscriptPath() {
   throw new Error(`Shared library file '${libyamlscriptName}' not found.`);
 }
 
-// Load libyamlscript shared library:
-const { symbols: {
-  graal_create_isolate,
-  load_ys_to_json,
-  graal_tear_down_isolate,
-} } = dlopen(findLibyamlscriptPath(), {
-  load_ys_to_json: { // (isolateThread, ysString) => jsonString
-    args: [FFIType.ptr, FFIType.cstring],
-    returns: FFIType.cstring,
-  },
-  graal_create_isolate: { // (options, noop?, isolateThread) => unknown?
-    args: [FFIType.ptr, FFIType.ptr, FFIType.ptr],
-    returns: FFIType.int,
-  },
-  graal_tear_down_isolate: { // (isolateThread) => unknown?
-    args: [FFIType.ptr],
-    returns: FFIType.int,
+
+
+
+
+
+const lib = koffi.load(findLibyamlscriptPath());
+
+const graal_create_isolate_params_t = koffi.struct(
+  "graal_create_isolate_params_t",
+  {
+    version: "int",
+    reserved_address_space_size: "long",
+    auxiliary_image_path: "char",
+    auxiliary_image_reserved_space_size: "long",
+
+    _reserved_1: "int",
+    _reserved_2: "char",
+    pkey: "int",
+    _reserved_3: "char",
+    _reserved_4: "char",
   }
+);
+const graal_isolate_t = koffi.struct("graal_isolate_t", {
+  version: "int",
+});
+const graal_isolatethread_t = koffi.struct("graal_isolatethread_t", {
+  version: "int",
 });
 
+const graal_create_isolate = lib.func(
+  // "int graal_create_isolate(graal_create_isolate_params_t* params, graal_isolate_t** isolate, graal_isolatethread_t** thread)"
+  // "int graal_create_isolate(void* params, int** isolate, int** thread)"
+  "int graal_create_isolate(void* params, void* isolate, int** thread)"
+);
+const graal_tear_down_isolate = lib.func(
+  "int graal_tear_down_isolate(int* isolateThread)"
+);
+const load_ys_to_json = lib.func(
+  "char* load_ys_to_json(long long int, const char*)"
+);
+
+
+
 export class YAMLScript {
-  private isolatethread: Uint8Array;
+  private isolate: Uint8Array;
+  private isolateThread: Uint8Array;
   private error?: null | {
-    cause: string,
-    type: string,
-    trace: string
+    cause: string;
+    type: string;
+    trace: string;
   };
 
   constructor() {
-    // this.isolatethread = Buffer.alloc(0);
-    this.isolatethread = new Uint8Array(8);
+    this.isolate = new Uint8Array(200);
+    this.isolateThread = new Uint8Array(200);
 
-    console.log('this.isolateThread: ', this.isolatethread)
-    console.log('pre graal_create_isolate')
-    graal_create_isolate(
-      null,
-      null,
-      this.isolatethread
+    console.log("pre graal_create_isolate:", this.isolate, this.isolateThread);
+    const createIsolateRet = graal_create_isolate(
+      undefined,
+      undefined,
+      this.isolateThread
     );
-    console.log('post graal_create_isolate:', this.isolatethread)
+    console.log(
+      "post graal_create_isolate:",
+      createIsolateRet,
+      this.isolate,
+      this.isolateThread
+    );
+
+    if (createIsolateRet !== 0) {
+      throw new Error("Failed to create isolate.");
+    }
   }
 
   load(input: string) {
     this.error = null;
 
-    console.log('input: ', input);
-    // FIXME: seg fault here
+    const inputBuffer = Buffer.alloc(20);
+    inputBuffer.fill(0);
+    inputBuffer.write(input, 0, "utf8");
+
+    console.log(
+      "input: ",
+      input,
+      Buffer.from(input, "utf8"),
+      ptr(Buffer.from(input, "utf8")),
+      inputBuffer
+    );
+    console.log("pre load");
     const dataJson = load_ys_to_json(
-      this.isolatethread,
-      ptr(Buffer.from(input, "utf8"))
+      this.isolateThread,
+      input
     );
 
-    console.log('string JSON', dataJson);
+    console.log("string JSON", dataJson);
     const resp = JSON.parse(dataJson.toString());
-    console.log('parsed JSON\'d resp:');
+    console.log("parsed JSON'd resp:", resp);
 
     this.error = resp.error;
     if (this.error) {
       throw new Error(this.error.cause);
     }
 
-    if (!('data' in resp)) {
+    if (!("data" in resp)) {
       throw new Error("Unexpected response from 'libyamlscript'");
     }
 
@@ -114,9 +150,9 @@ export class YAMLScript {
   }
 
   destructor() {
-    console.log('pre_graal_tear_down_isolate');
-    const ret = graal_tear_down_isolate(this.isolatethread);
-    if (ret !== 0) {
+    console.log("pre_graal_tear_down_isolate");
+    const tearDownIsolateRet = graal_tear_down_isolate(this.isolateThread);
+    if (tearDownIsolateRet !== 0) {
       throw new Error("Failed to tear down isolate.");
     }
   }
@@ -127,15 +163,16 @@ export function load(input: string) {
   const ys = new YAMLScript();
   try {
     return ys.load(input);
+  } catch (e) {
+    console.error(e);
+    throw e;
   } finally {
-    console.log("pre destructor")
+    console.log("pre destructor");
     ys.destructor();
   }
 }
 
-// Example usage:
 
-const yamlFilePath = path.join(__dirname, './../sample/data.ys');
-const yamlStr = fs.readFileSync(yamlFilePath, 'utf8');
+const yamlStr = "a: 1";
 console.log(yamlStr);
-console.log(load(yamlStr))
+console.log(load(yamlStr));
