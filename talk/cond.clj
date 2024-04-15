54769b20a870913965e3e32a316d76f9112dae95
Ingy döt Net <ingy@ingy.net>
2024-04-15 07:35:45 -0700

    ...

--- talk/cond.clj
+++
@@ -1,8 +0,0 @@
-(defn foo []
-  (let [a (reduce + (rng 1 (rand-int 8)))]
-    (cond
-      (> a 20) "yes"
-      (> a 10) "no"
-      :else "maybe")))
-
-(+++ (each [i (rng 1 10)] (say (foo))))
--- talk/multi-doc.clj
+++
@@ -1,9 +0,0 @@
-(+++ {"foo" "bar",
-      "food" ["apple" "banana" "carrot"]})
-
-(+++ {"foo" 123, "bar" (rng 5 10)})
-
-(+++ (say
-       (yaml/dump
-         {"$" ($), "$#" @$#, "$$" @$$})))
-
