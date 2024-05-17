783604b357e0135839c066a82da506c1ff070816
Joao Paulo Magalhaes <dev@jpmag.me>
2024-05-17 20:52:31 +0100

    rapidyaml->edn pt1: create handler code (YAML evts)

--- rapidyaml/native/rapidyaml_events.cpp
+++ rapidyaml/native/rapidyaml_events.cpp
@@ -1,6 +1,6 @@
 #define RYML_SINGLE_HDR_DEFINE_NOW
 #include <ryml_all.hpp>
-#include <test_suite/test_suite_event_handler.hpp>
+#include "rapidyaml_edn_handler.hpp"
 #include <string.h>
 
 namespace ryml {
@@ -13,15 +13,15 @@ using namespace c4::yml;
 extern "C" {
 #endif
 
-RYML_EXPORT void ys2evts_destroy(char *events)
+RYML_EXPORT void ys2edn_destroy(char *events)
 {
     ryml::Callbacks const& cb = ryml::get_callbacks();
     cb.m_free(events, strlen(events), cb.m_user_data);
 }
 
-RYML_EXPORT char * ys2evts_create(char *src)
+RYML_EXPORT char * ys2edn_create(char *src)
 {
-    const std::string events = ryml::emit_events_from_source(ryml::to_substr(src));
+    const std::string events = ryml::emit_edn_from_source(ryml::to_substr(src));
     const size_t sz = events.size();
     ryml::Callbacks const& cb = ryml::get_callbacks();
     void *mem = cb.m_allocate(1 + sz, nullptr, cb.m_user_data);
