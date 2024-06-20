d51a82dd1851956eea0fde6b0939a30d6eddc7cb
Andrew Pam <andrew@sericyb.com.au>
2024-06-21 01:30:16 +1000

    go: Add the Go binding library for libyamlscript

---
+++ go/yamlscript_test.go
@@ -0,0 +1,17 @@
+package yamlscript_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/yaml/yamlscript/go"
+)
+
+func TestYamlScript(t *testing.T) {
+	_, err := yamlscript.Load(":")
+	assert.Error(t, err)
+
+	data, err := yamlscript.Load("!yamlscript/v0/data\ntest:: inc(41)")
+	assert.NoError(t, err)
+	assert.Equal(t, map[string]any{"test": (float64)(42)}, data)
+}
