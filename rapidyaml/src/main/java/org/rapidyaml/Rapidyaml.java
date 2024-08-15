a693eee2782c88cad20de00b1a41535a74453704
Ingy döt Net <ingy@ingy.net>
2024-08-15 11:09:53 -0700

    ...

--- rapidyaml/src/main/java/org/rapidyaml/Rapidyaml.java
+++ rapidyaml/src/main/java/org/rapidyaml/Rapidyaml.java
@@ -1,7 +1,5 @@
 package org.rapidyaml;
 
-import com.sun.jna.Pointer;
-import com.sun.jna.Native;
 import java.nio.charset.StandardCharsets;
 import java.nio.ByteBuffer;
 
@@ -13,20 +11,31 @@ public class Rapidyaml
 {
     public static String RAPIDYAML_VERSION = "0.7.0";
 
-    private final ILibRapidyaml librapidyaml;
-    private final Pointer ryml2edn;
+    private native Object ys2edn_init();
+    private native void ys2edn_destroy(Object ryml2edn);
+    private native int ys2edn(Object ryml2edn, String filename,
+               byte[] ys, int ys_length,
+               byte[] edn, int edn_length);
+    private native int ys2edn_retry_get(Object ryml2edn, byte[] edn, int edn_size);
+
+
+
+
+
+
+    private final Object ryml2edn;
 
     public Rapidyaml()
     {
-        this.librapidyaml = LibRapidyaml.library();
-        this.ryml2edn = this.librapidyaml.ys2edn_init();
+        System.loadLibrary("rapidyaml");
+        this.ryml2edn = this.ys2edn_init();
     }
 
     protected void finalize() throws Throwable
     {
         try
         {
-            this.librapidyaml.ys2edn_destroy(this.ryml2edn);
+            this.ys2edn_destroy(this.ryml2edn);
         }
         finally
         {
@@ -34,33 +43,21 @@ public class Rapidyaml
         }
     }
 
-    public String parseYS0(String srcstr) throws RuntimeException
-    {
-        // using ys2edn_alloc(): allocates the output in C++
-        String filename = "yamlscript"; // fixme
-        // see https://stackoverflow.com/questions/34159610/jna-how-to-pass-a-string-as-void-from-java-to-c
-        // 1. works:
-        //return librapidyaml.ys2edn_alloc(this.ryml2edn, filename, srcstr, srcstr.length());
-        // 2. crashes:
-        //byte[] src = Native.toByteArray(srcstr, StandardCharsets.UTF_8);
-        //return librapidyaml.ys2edn_alloc(this.ryml2edn, filename, src, src.length);
-        // 3. works:
-        byte[] src = srcstr.getBytes(StandardCharsets.UTF_8);
-        return librapidyaml.ys2edn_alloc(this.ryml2edn, filename, src, src.length);
-    }
-
     public String parseYS(String srcstr) throws RuntimeException
     {
+
         String filename = "yamlscript"; // fixme
         byte[] src = srcstr.getBytes(StandardCharsets.UTF_8);
         int edn_size = 10 * src.length;
+
         byte[] edn = new byte[edn_size];
-        int required_size = librapidyaml.ys2edn(this.ryml2edn, filename, src, src.length, edn, edn_size);
+        int required_size = ys2edn(
+            this.ryml2edn, filename, src, src.length, edn, edn_size);
         if(required_size > edn_size)
         {
             edn_size = required_size;
             edn = new byte[edn_size];
-            required_size = librapidyaml.ys2edn_retry_get(this.ryml2edn, edn, edn_size);
+            required_size = ys2edn_retry_get(this.ryml2edn, edn, edn_size);
             if(required_size != edn_size)
             {
                 throw new RuntimeException("inconsistent size");
