88a2871591bfdde488abced16227c523313cac51
Ingy döt Net <ingy@ingy.net>
2024-09-05 09:19:53 -0700

    core: Change dot debug from .??? to .>

--- core/src/yamlscript/ysreader.clj
+++ core/src/yamlscript/ysreader.clj
@@ -341,7 +341,7 @@
     ".#--" ["count(" ")" "." "dec(" ")"]
     ".#++" ["count(" ")" "." "inc(" ")"]
 
-    ".???" ["DBG(" ")"]))
+    ".>"   ["DBG(" ")"]))
 
 (defn read-scalar [[token & tokens]]
   (cond
