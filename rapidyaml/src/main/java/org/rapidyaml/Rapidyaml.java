43931330831c7393ec3759c55e2b319e70561f12
Joao Paulo Magalhaes <dev@jpmag.me>
2024-09-15 13:36:58 +0100

    sketch

--- rapidyaml/src/main/java/org/rapidyaml/Rapidyaml.java
+++ rapidyaml/src/main/java/org/rapidyaml/Rapidyaml.java
@@ -41,6 +41,53 @@ public class Rapidyaml {
         }
     }
 
+    /*
+    using FlagsType = int32_t;
+    enum : FlagsType {
+        // ---------------------
+        // scalar flags
+        SCLR = 1 <<  0,   // =VAL
+        PLAI = 1 <<  1,   // : (plain scalar)
+        SQUO = 1 <<  2,   // ' (single-quoted scalar)
+        DQUO = 1 <<  3,   // " (double-quoted scalar)
+        LITL = 1 <<  4,   // | (block literal scalar)
+        FOLD = 1 <<  5,   // > (block folded scalar)
+        // ---------------------
+        // container flags
+        BSEQ = 1 <<  6,   // +SEQ (Begin SEQ)
+        ESEQ = 1 <<  7,   // -SEQ (End   SEQ)
+        BMAP = 1 <<  8,   // +MAP
+        EMAP = 1 <<  9,   // -MAP
+        FLOW = 1 << 10,   // flow container: [] for seqs or {} for maps
+        BLCK = 1 << 11,   // block container
+        // ---------------------
+        // document flags
+        BDOC = 1 << 12,   // +DOC
+        EDOC = 1 << 13,   // -DOC
+        BSTR = 1 << 14,   // +STR
+        ESTR = 1 << 15,   // -STR
+        EXPL = 1 << 16,   // --- (with BDOC) or ... (with EDOC) (may be fused with FLOW if needed)
+        // ---------------------
+        // other flags
+        ALIA = 1 << 19    // ref
+        ANCH = 1 << 18,   // anchor
+        TAG_ = 1 << 20,   // tag
+    }
+    struct ParseEvent
+    {
+        FlagsType flags;
+        int32_t strStart;
+        int32_t strEnd;
+        // anchors: either this:
+        int32_t anchorStart;
+        int32_t anchorEnd;
+        // ... or this, which saves space when anchors are rare,
+        // and has the advantage of making the event have 4 words:
+        //int32_t anchorId; // counter, pointing at a different
+                            // array of { int32_t anchorStart, anchorEnd; }
+    };
+    */
+
     public String parseYS(String srcstr) throws RuntimeException, org.rapidyaml.YamlParseErrorException {
         String filename = "yamlscript"; // fixme
         byte[] src = srcstr.getBytes(StandardCharsets.UTF_8);
