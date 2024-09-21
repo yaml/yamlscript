397c8f49cda3886cbbb5bfc21c3b7cfbee0bb9a7
Ingy döt Net <ingy@ingy.net>
2024-09-21 15:12:15 -0700

    core: Turn regex into function for certain dot functions

--- core/src/ys/poly.clj
+++ core/src/ys/poly.clj
@@ -3,6 +3,9 @@
 
 (ns ys.poly)
 
+(defn- regex? [x]
+  (= (type x) java.util.regex.Pattern))
+
 (defn ++map [a b]
   (if (and (ifn? b) (or (sequential? a) (string? a)))
     (map b a)
@@ -12,6 +15,16 @@
   (if (and (ifn? b) (or (sequential? a) (string? a)))
     (mapv b a)
     (mapv a b)))
+
+(defn- regex-to-fn [a b f]
+  (let [a (if (regex? a) #(re-find a %1) a)]
+    (f a b)))
+
+(defn ++filter [a b] (regex-to-fn a b filter))
+(defn ++filterv [a b] (regex-to-fn a b filterv))
+(defn ++keep [a b] (regex-to-fn a b keep))
+(defn ++remove [a b] (regex-to-fn a b remove))
+(defn ++take-while [a b] (regex-to-fn a b take-while))
 
 
 ;;------------------------------------------------------------------------------
@@ -37,6 +50,14 @@
          (apply ~name a# xs#)))))
 
 (defmacro ^:private -seq-2nd+ [name]
+  (let [dname (symbol (str "+" name))
+        pname (symbol (str "++" name))]
+    `(defn ~dname [a# b#]
+       (if (coll? b#)
+         (~pname a# b#)
+         (~pname b# a#)))))
+
+(defmacro ^:private -seq-rgx [name]
   (let [dname (symbol (str "+" name))
         pname (symbol (str "++" name))]
     `(defn ~dname [a# b#]
@@ -76,10 +97,10 @@
 (-seq-2nd drop-last)
 (-seq-2nd drop-while)
 (-seq-2nd every?)
-(-seq-2nd filter)
-(-seq-2nd filterv)
+(-seq-rgx filter)
+(-seq-rgx filterv)
 (-seq-2nd interpose)
-(-seq-2nd keep)
+(-seq-rgx keep)
 (-seq-2nd+ map)
 (-seq-2nd mapcat)
 (-seq-2nd+ mapv)
@@ -91,7 +112,7 @@
 (-rgx-1st re-matches)
 (-rgx-1st re-seq)
 (-seq-last reduce)
-(-seq-2nd remove)
+(-seq-rgx remove)
 (-seq-2nd repeat)
 #_(-rgx-2nd replace)
 (-seq-2nd some)
@@ -99,7 +120,7 @@
 (-seq-2nd split-with)
 (-seq-2nd take)
 (-seq-2nd take-last)
-(-seq-2nd take-while)
+(-seq-rgx take-while)
 (-clj-mac when)
 
 (comment
