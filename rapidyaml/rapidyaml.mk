f08cc574dd9ba4cbe397511f6e7d09eb5593fa2b
Joao Paulo Magalhaes <dev@jpmag.me>
2024-12-27 15:18:56 +0000

    rapidyaml static linking

--- rapidyaml/rapidyaml.mk
+++ rapidyaml/rapidyaml.mk
@@ -1,4 +1,5 @@
-RAPIDYAML_TAG ?= v0.7.2
+RAPIDYAML_VERSION ?= 0.7.2
+RAPIDYAML_TAG ?= v$(RAPIDYAML_TAG)
 RAPIDYAML_REPO := https://github.com/biojppm/rapidyaml
 RAPIDYAML_JAVA := \
   $(ROOT)/rapidyaml/src/main/java/org/rapidyaml/Rapidyaml.java \
