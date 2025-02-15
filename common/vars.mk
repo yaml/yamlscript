223558a206e55b8b6481f6f882691525c9954fbd
Joao Paulo Magalhaes <dev@jpmag.me>
2025-02-15 15:23:42 +0000

    rapidyaml: Update to 0.8.0

--- common/vars.mk
+++ common/vars.mk
@@ -1,24 +1,27 @@
-include $(MAKES)/graalvm.mk
-include $(MAKES)/maven.mk
-# This is the previously released `ys` version suitable for internal tasks:
-YS-VERSION := 0.1.97
-include $(MAKES)/ys.mk
+export LANG := en_US.UTF-8
 
-export YS_TMPDIR := $(LOCAL-TMP)
-export TMPDIR := $(LOCAL-TMP)
-export TEMP := $(TMPDIR)
-export TMP := $(TMPDIR)
+YS_TMP ?= /tmp/yamlscript
 
-YS-REPO-URL := https://github.com/yaml/yamlscript
-YS-GH-API-URL := https://api.github.com/repos/yaml/yamlscript
+ifeq (,$(wildcard $(YS_TMP)))
+  $(shell mkdir -p $(YS_TMP))
+endif
 
-YS-INSTALL-URL := https://yamlscript.org/install
+BUILD_BIN := $(YS_TMP)/bin
+
+BUILD_BIN_YS_VERSION := 0.1.93
+
+BUILD_BIN_YS := $(BUILD_BIN)/ys-$(BUILD_BIN_YS_VERSION)
+
+YS_REPO_URL := https://github.com/yaml/yamlscript
+YS_GH_API_URL := https://api.github.com/repos/yaml/yamlscript
+
+YS_INSTALL_URL := https://yamlscript.org/install
 
 COMMON := $(ROOT)/common
 
 unexport YS_FORMATTER
 
-override PATH := $(ROOT)/util:$(ROOT)/ys/bin:$(LOCAL-BIN):$(PATH)
+export PATH := $(ROOT)/util:$(ROOT)/ys/bin:$(BUILD_BIN):$(PATH)
 
 export YAMLSCRIPT_ROOT ?= $(ROOT)
 
@@ -30,20 +33,43 @@ ifdef v
   export TEST_VERBOSE := 1
 endif
 
-ifdef IS-LINUX
+ostype := $(shell /bin/bash -c 'echo $$OSTYPE')
+machtype := $(shell /bin/bash -c 'echo $$MACHTYPE')
+
+ifneq (,$(findstring x86_64,$(machtype)))
+  IS_INTEL := true
+else ifneq (,$(findstring arm64,$(machtype)))
+  IS_ARM := true
+else ifneq (,$(findstring aarch64,$(machtype)))
+  IS_ARM := true
+endif
+
+ifneq (,$(findstring linux,$(ostype)))
+  IS_LINUX := true
   GCC := gcc -std=gnu99 -fPIC -shared
   SO := so
   DY :=
-else ifdef IS-MACOS
+  DOTLIB := a
+else ifneq (,$(findstring darwin,$(ostype)))
+  IS_MACOS := true
   GCC := gcc -dynamiclib
   SO := dylib
   DY := DY
+  DOTLIB := a
 else
-  $(error Unsupported OSTYPE: $(OS-TYPE))
+  DOTLIB := lib
+  $(error Unsupported OSTYPE: $(ostype))
+endif
+
+IS_ROOT ?= undefined
+ifneq (false,$(IS_ROOT))
+ifeq (0,$(shell id -u))
+  IS_ROOT := true
+endif
 endif
 
 LIBZ := false
-ifdef IS-MACOS
+ifeq (true,$(IS_MACOS))
   LIBZ := true
 else
 # Fix https://github.com/yaml/yamlscript/issues/210
@@ -57,24 +83,10 @@ endif
 endif
 
 CURL := $(shell command -v curl)
-ifdef CURL
-ifdef YS_QUIET
-  CURL := $(CURL) -sSL
-else
-  CURL := $(CURL) -SL
-endif
-endif
-
-define need-curl
-	@[[ "$(CURL)" ]] || { \
-	  echo "*** 'curl' is required but not installed"; \
-	  exit 1; \
-	}
-endef
 
 TIME := time -p
 
-ifeq (true,$(IS-ROOT))
+ifeq (true,$(IS_ROOT))
   PREFIX ?= /usr/local
 else
   PREFIX ?= $(HOME)/.local
@@ -82,43 +94,126 @@ endif
 
 
 #------------------------------------------------------------------------------
-# Set JAVA and GRAALVM variables:
+# Set machine specific variables:
 #------------------------------------------------------------------------------
+ifeq (true,$(IS_LINUX))
+  GRAALVM_SUBDIR :=
 
-JAVA-INSTALLED := $(GRAALVM) $(MAVEN)
+  ifeq (true,$(IS_INTEL))
+    GRAALVM_ARCH := linux-x64
 
-GRAALVM-O ?= 1
-# qbm is Quick Build Mode
-ifdef qbm
-  GRAALVM-O := b
+  else ifeq (true,$(IS_ARM))
+    GRAALVM_ARCH := linux-aarch64
+
+  else
+    $(error Unsupported Linux MACHTYPE: $(machtype))
+  endif
+
+else ifeq (true,$(IS_MACOS))
+  GRAALVM_SUBDIR := /Contents/Home
+
+  ifeq (true,$(IS_ARM))
+    GRAALVM_ARCH := macos-aarch64
+
+  else ifeq (true,$(IS_INTEL))
+    GRAALVM_ARCH := macos-x64
+
+  else
+    $(error Unsupported MacOS MACHTYPE: $(machtype))
+  endif
+
+else
+  $(error Unsupported OSTYPE: $(ostype))
 endif
 
 
 #------------------------------------------------------------------------------
-# Set LEIN variables:
+# Set GRAALVM variables:
 #------------------------------------------------------------------------------
 
-export LEIN_HOME := $(LOCAL-HOME)/lein
+### For Orable GraalVM No-Fee #################################################
+ifndef GRAALVM_CE
+GRAALVM_SRC := https://download.oracle.com/graalvm
+GRAALVM_VER ?= 23
+GRAALVM_TAR := graalvm-jdk-$(GRAALVM_VER)_$(GRAALVM_ARCH)_bin.tar.gz
+GRAALVM_URL := $(GRAALVM_SRC)/$(GRAALVM_VER)/latest/$(GRAALVM_TAR)
+GRAALVM_PATH ?= $(YS_TMP)/graalvm-oracle-$(GRAALVM_VER)
+
+### For GraalVM CE (Community Edition) ########################################
+else
+GRAALVM_SRC := https://github.com/graalvm/graalvm-ce-builds/releases/download
+GRAALVM_VER ?= 21
+  ifeq (21,$(GRAALVM_VER))
+    override GRAALVM_VER := jdk-21.0.0
+  endif
+  ifeq (17,$(GRAALVM_VER))
+    override GRAALVM_VER := jdk-17.0.8
+  endif
+GRAALVM_TAR := graalvm-community-$(GRAALVM_VER)_$(GRAALVM_ARCH)_bin.tar.gz
+GRAALVM_URL := $(GRAALVM_SRC)/$(GRAALVM_VER)/$(GRAALVM_TAR)
+GRAALVM_PATH ?= $(YS_TMP)/graalvm-ce-$(GRAALVM_VER)
+endif
+
+GRAALVM_HOME := $(GRAALVM_PATH)$(GRAALVM_SUBDIR)
+GRAALVM_DOWNLOAD := $(YS_TMP)/$(GRAALVM_TAR)
+GRAALVM_INSTALLED := $(GRAALVM_HOME)/release
+
+GRAALVM_O ?= 1
+# qbm is Quick Build Mode
+ifdef qbm
+  GRAALVM_O := b
+endif
+
+
+#------------------------------------------------------------------------------
+# Set MAVEN variables:
+#------------------------------------------------------------------------------
+
+MAVEN_VER := 3.9.6
+MAVEN_SRC := https://dlcdn.apache.org/maven/maven-3/$(MAVEN_VER)/binaries
+MAVEN_TAR := apache-maven-$(MAVEN_VER)-bin.tar.gz
+MAVEN_URL := $(MAVEN_SRC)/$(MAVEN_TAR)
+
+MAVEN_HOME := $(YS_TMP)/apache-maven-$(MAVEN_VER)
+MAVEN_DOWNLOAD := $(YS_TMP)/$(MAVEN_TAR)
+MAVEN_INSTALLED := $(MAVEN_HOME)/bin/mvn
+
+# XXX Not always working yet:
+# export M2_HOME := $(YS_TMP)/.m2
+export M2_HOME := $(HOME)/.m2
+
+MAVEN_REPOSITORY := $(M2_HOME)/repository
+MAVEN_SETTINGS := $(M2_HOME)/conf/settings.xml
+
+# XXX .m2 in tmp not working yet:
+# export MAVEN_OPTS := \
+#   -Duser.home=$(YS_TMP) \
+#   -Dmaven.repo.local=$(MAVEN_REPOSITORY) \
+export MAVEN_OPTS := \
+  -Duser.home=$(HOME) \
+  -Dmaven.repo.local=$(MAVEN_REPOSITORY) \
+
+export PATH := $(MAVEN_HOME)/bin:$(PATH)
+
 export LEIN_JVM_OPTS := \
   -XX:+TieredCompilation \
   -XX:TieredStopAtLevel=1 \
-  $(MAVEN-OPTS)
+  $(MAVEN_OPTS)
+
+# XXX Can't use MAVEN_SETTINGS until /tmp/yamlscript/.m2 is working:
+# JAVA_INSTALLED := $(GRAALVM_INSTALLED) $(MAVEN_INSTALLED) $(MAVEN_SETTINGS)
+JAVA_INSTALLED := $(GRAALVM_INSTALLED) $(MAVEN_INSTALLED)
 
 
 #------------------------------------------------------------------------------
 # Set release asset variables:
 #------------------------------------------------------------------------------
 
-OA-linux-arm64 := linux-aarch64
-OA-linux-int64 := linux-x64
-OA-macos-arm64 := macos-aarch64
-OA-macos-int64 := macos-x64
+RELEASE_YS_NAME := ys-$(YS_VERSION)-$(GRAALVM_ARCH)
+RELEASE_YS_TAR := $(RELEASE_YS_NAME).tar.xz
 
-RELEASE-YS-NAME := ys-$(YS_VERSION)-$(OA-$(OS-ARCH))
-RELEASE-YS-TAR := $(RELEASE-YS-NAME).tar.xz
-
-RELEASE-LYS-NAME := libys-$(YS_VERSION)-$(OA-$(OS-ARCH))
-RELEASE-LYS-TAR := $(RELEASE-LYS-NAME).tar.xz
+RELEASE_LYS_NAME := libyamlscript-$(YS_VERSION)-$(GRAALVM_ARCH)
+RELEASE_LYS_TAR := $(RELEASE_LYS_NAME).tar.xz
 
 
 #------------------------------------------------------------------------------
@@ -126,10 +221,18 @@ RELEASE-LYS-TAR := $(RELEASE-LYS-NAME).tar.xz
 #------------------------------------------------------------------------------
 RAPIDYAML := $(ROOT)/rapidyaml
 
-RAPIDYAML_VERSION := 0.7.2
+RAPIDYAML_VERSION := 0.8.0
+RAPIDYAML_TAG ?= v$(RAPIDYAML_VERSION)
+RAPIDYAML_REPO := https://github.com/biojppm/rapidyaml
+RAPIDYAML_BUILD_TYPE := Release
+RAPIDYAML_DBG := 0
+RAPIDYAML_TIMED := 0
+RAPIDYAML_JAVA := \
+  $(ROOT)/rapidyaml/src/main/java/org/rapidyaml/Rapidyaml.java \
+  $(ROOT)/rapidyaml/src/main/java/org/rapidyaml/YamlParseErrorException.java
+RAPIDYAML_SO := $(ROOT)/rapidyaml/native/librapidyaml.$(RAPIDYAML_VERSION).$(SO)
+RAPIDYAML_LIB := $(ROOT)/rapidyaml/native/librapidyaml.$(DOTLIB)
 RAPIDYAML_JAR := $(ROOT)/rapidyaml/target/rapidyaml-$(RAPIDYAML_VERSION).jar
-RAPIDYAML_SO := \
-  $(ROOT)/rapidyaml/native/librapidyaml.$(RAPIDYAML_VERSION).$(SO)
 RAPIDYAML_INSTALLED_DIR := \
   $(MAVEN_REPOSITORY)/org/rapidyaml/rapidyaml/$(RAPIDYAML_VERSION)/
 RAPIDYAML_INSTALLED := \
@@ -138,3 +241,14 @@ RAPIDYAML_INSTALLED := \
 
 #------------------------------------------------------------------------------
 default::
+
+build-bin-ys: $(BUILD_BIN_YS)
+
+$(BUILD_BIN_YS):
+	curl -sSL $(YS_INSTALL_URL) | \
+	  PREFIX=$$(dirname $(BUILD_BIN)) \
+	  VERSION=$(BUILD_BIN_YS_VERSION) \
+	  BIN=1 bash
+
+$(BUILD_BIN):
+	mkdir -p $@
