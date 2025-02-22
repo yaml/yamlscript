f8c46637cde14d9506e10cb0773f43e6074c397f
Joao Paulo Magalhaes <dev@jpmag.me>
2025-02-22 19:53:07 +0000

    rapidyaml: Test parsing yamllm.ys

--- rapidyaml/src/main/java/org/rapidyaml/cmp/run.sh
+++ rapidyaml/src/main/java/org/rapidyaml/cmp/run.sh
@@ -9,7 +9,10 @@ rymldir=$(cd $nativedir/.. ; pwd)
 make -C $nativedir build RAPIDYAML_TIMED=1
 make -C $rymldir test RAPIDYAML_TIMED=1
 
+
 cd $thisdir
+wget https://raw.githubusercontent.com/yaml/yamllm/refs/heads/main/bin/yamllm.ys
+ls -lFhp
 jd=${jd:-/usr/lib/jvm/java-23-openjdk/bin}
 $jd/javac -d . ../*.java
 $jd/javac -d . -cp . CmpEdnEvt.java
