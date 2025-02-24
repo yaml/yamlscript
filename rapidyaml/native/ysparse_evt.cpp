586f7bf742900366f235144240c100576357f1b7
Joao Paulo Magalhaes <dev@jpmag.me>
2025-02-24 00:59:52 +0000

    rapidyaml: Rename ryml2evt/ys2evt to ysparse

--- rapidyaml/native/ysparse_evt.cpp
+++ rapidyaml/native/ysparse_evt.cpp
@@ -10,7 +10,7 @@ extern "C" {
 // https://stackoverflow.com/questions/4138168/what-happens-when-i-throw-a-c-exception-from-a-native-java-method
 
 namespace {
-C4_NORETURN void ys2evt_parse_error(const char* msg, size_t msg_len, Location location, void *user_data)
+C4_NORETURN void ysparse_error(const char* msg, size_t msg_len, Location location, void *user_data)
 {
     YsParseError exc;
     exc.location = location;
@@ -19,43 +19,43 @@ C4_NORETURN void ys2evt_parse_error(const char* msg, size_t msg_len, Location lo
 }
 } // anon namespace
 
-RYML_EXPORT Ryml2Evt *ys2evt_init()
+RYML_EXPORT ysparse *ysparse_init()
 {
-    TIMED_SECTION("ys2evt_init");
+    TIMED_SECTION("cpp:ysparse_init");
     Callbacks cb = {};
-    cb.m_error = &ys2evt_parse_error;
+    cb.m_error = &ysparse_error;
     set_callbacks(cb);
-    Ryml2Evt *ryml2evt = _RYML_CB_ALLOC(get_callbacks(), Ryml2Evt, 1);
+    ysparse *ryml2evt = _RYML_CB_ALLOC(get_callbacks(), ysparse, 1);
     _RYML_CB_CHECK(get_callbacks(), ryml2evt != nullptr);
-    new ((void*)ryml2evt) Ryml2Evt();
+    new ((void*)ryml2evt) ysparse();
     return ryml2evt;
 }
 
-RYML_EXPORT void ys2evt_destroy(Ryml2Evt *ryml2evt)
+RYML_EXPORT void ysparse_destroy(ysparse *obj)
 {
-    TIMED_SECTION("ys2evt_destroy");
-    ryml2evt->~Ryml2Evt();
-    _RYML_CB_FREE(get_callbacks(), ryml2evt, Ryml2Evt, 1);
+    TIMED_SECTION("cpp:ysparse_destroy");
+    obj->~ysparse();
+    _RYML_CB_FREE(get_callbacks(), obj, ysparse, 1);
 }
 
-RYML_EXPORT size_type ys2evt_parse(Ryml2Evt *ryml2evt,
-                                   const char *filename,
-                                   char *ys, size_type ys_size,
-                                   evt::DataType *events, size_type evt_size)
+RYML_EXPORT size_type ysparse_parse(ysparse *obj,
+                                    const char *filename,
+                                    char *ys, size_type ys_size,
+                                    evt::DataType *events, size_type evt_size)
 {
-    TIMED_SECTION("ys2evt_parse", ys_size);
+    TIMED_SECTION("cpp:ysparse", ys_size);
     csubstr filename_ = filename ? to_csubstr(filename) : csubstr{};
     substr ys_(ys, (size_t)ys_size);
     {
-        TIMED_SECTION("reset + reserve");
-        ryml2evt->reset(ys_, events, evt_size);
-        ryml2evt->m_handler.reserve(256u);
+        TIMED_SECTION("cpp:ysparse/reset");
+        obj->reset(ys_, events, evt_size);
+        obj->m_handler.reserve(256u);
     }
     {
-        TIMED_SECTION("parse_in_place", ys_size);
-        ryml2evt->m_parser.parse_in_place_ev(filename_, ys_);
+        TIMED_SECTION("cpp:ysparse/parse", ys_size);
+        obj->m_parser.parse_in_place_ev(filename_, ys_);
     }
-    return (size_type)ryml2evt->m_handler.m_evt_curr;
+    return (size_type)obj->m_handler.m_evt_curr;
 }
 
 #if defined(__cplusplus)
