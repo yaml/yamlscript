8e94f40532df19688e7f36a6b09c86662253c63a
Ingy döt Net <ingy@ingy.net>
2025-03-08 11:48:01 -0800

    ...

---
+++ rapidyaml/src/main/java/org/rapidyaml/NativeLibLoader.java
@@ -0,0 +1,87 @@
+package org.rapidyaml;
+
+import java.io.*;
+import java.nio.file.FileSystemNotFoundException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.ProviderNotFoundException;
+import java.nio.file.StandardCopyOption;
+
+public class NativeLibLoader {
+    private static File tempdir;
+
+    private NativeLibLoader() {}
+
+    public static void loadLibraryFromJar(String path) throws IOException {
+        System.out.printf("1: >>>%s<<<\n", path);
+
+//       if (null == path || !path.startsWith("/")) {
+//           throw new IllegalArgumentException(
+//               "The path has to be absolute (start with '/').");
+//       }
+
+//       String[] parts = path.split("/");
+//       String filename = (parts.length > 1) ? parts[parts.length - 1] : null;
+//       System.out.printf("jar path file = %s\n", filename);
+
+        if (tempdir == null) {
+            tempdir = createTempDirectory("rapidyamllibloader");
+            tempdir.deleteOnExit();
+        }
+        System.out.printf("2: >>>%s<<<\n", tempdir);
+
+//       File temp = new File(tempdir, filename);
+        File temp = new File(tempdir, path);
+
+        System.out.printf("3: >>>%s<<<\n", path);
+        try (InputStream is = NativeLibLoader.class.getResourceAsStream(path)) {
+            Files.copy(is, temp.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        }
+        catch (IOException e) {
+            temp.delete();
+            throw e;
+        }
+        catch (NullPointerException e) {
+            temp.delete();
+            throw new FileNotFoundException(
+                "File " + path + " was not found inside JAR.");
+        }
+        System.out.printf("4: >>>%s<<<\n", path);
+
+        try {
+            System.load(temp.getAbsolutePath());
+        }
+        finally {
+            if (isPosixCompliant()) {
+                temp.delete();
+            }
+            else {
+                temp.deleteOnExit();
+            }
+        }
+    }
+
+    private static boolean isPosixCompliant() {
+        try {
+            return FileSystems.getDefault()
+                    .supportedFileAttributeViews()
+                    .contains("posix");
+        }
+        catch (FileSystemNotFoundException
+                | ProviderNotFoundException
+                | SecurityException e) {
+            return false;
+        }
+    }
+
+    private static File createTempDirectory(String prefix) throws IOException {
+        String tempDir = System.getProperty("java.io.tmpdir");
+        File generatedDir = new File(tempDir, prefix + System.nanoTime());
+
+        if (!generatedDir.mkdir())
+            throw new IOException(
+                "Failed to create temp directory " + generatedDir.getName());
+
+        return generatedDir;
+    }
+}
