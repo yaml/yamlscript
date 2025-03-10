df70cf36087ca8a38f6f2f041f5cbde17b782eab
Ingy döt Net <ingy@ingy.net>
2025-03-09 18:37:33 -0700

    ...

---
+++ mvp/src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+// import org.apache.commons.lang3.StringUtils;
+
+public class App {
+    public static void main(String[] args) {
+        String text = "Hello, World!";
+        System.out.println("Original text: " + text);
+        // System.out.println("Reversed text: " + StringUtils.reverse(text));
+    }
+}
