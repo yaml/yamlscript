1226e9bfd7e17ac5a0f848928c5ea8c0e0b43d22
Edward Brown <innovative.fowler@mask.pro.fmtr.dev>
2025-05-23 09:15:17 +0100

    Add custom build_ext class for platform-specific wheel tagging
    Introduce `LibYAMLScriptExtensionBuilder` to ensure setuptools correctly tags wheels with platform/architecture-specific metadata.

--- python/setup.py
+++ python/setup.py
@@ -1,6 +1,7 @@
 import pathlib
 
-from setuptools import setup
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
 
 from lib.yamlscript import NAME, yamlscript_version as version, get_libyamlscript_name
 
@@ -10,6 +11,25 @@ root = pathlib.Path(__file__).parent.resolve()
 long_description = \
   (root / '.long_description.md') \
   .read_text(encoding='utf-8')
+
+
+class LibYAMLScriptExtensionBuilder(build_ext):
+  """
+
+  The shared library is pre-built, but we need to provide setuptools
+   with a dummy extension builder, so that it knows that the wheels
+   aren't just pure-Python and tags them with the correct
+   platform/architecture-specific naming and metadata.
+
+  """
+
+  def build_extensions(self):
+    """
+
+    Build nothing.
+
+    """
+    pass
 
 setup(
   name=NAME,
@@ -24,11 +44,16 @@ setup(
   packages=[NAME],
   package_dir = {'': 'lib'},
 
+  ext_modules=[
+    Extension(name=NAME, sources=[])
+  ],
+  cmdclass={
+    'build_ext': LibYAMLScriptExtensionBuilder,
+  },
+
   package_data={
     NAME: [libyamlscript_name],
   },
-
-  python_requires = '>=3.6, <4',
   install_requires = [
     'pyyaml',
   ],
