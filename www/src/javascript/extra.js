cb92cf82503f0b4e9466379b078f47d123be900a
Yumi Chen <27608365+YumiChen@users.noreply.github.com>
2025-06-18 22:41:29 -0400

    www: Enhance sidebar accessibility and interaction handling

---
+++ www/src/javascript/extra.js
@@ -0,0 +1,26 @@
+document$.subscribe(function () {
+  const sidebarControl = document.querySelector(
+    ".md-toggle[data-md-toggle='drawer']"
+  );
+
+  // Disable interaction with header, content, and footer when the sidebar
+  // drawer is open.
+  sidebarControl.addEventListener("change", (e) => {
+    const selectors = [
+      "header",
+      ".md-content",
+      ".md-sidebar[data-md-type='toc']",
+      "footer",
+    ];
+    const isChecked = e.target.checked;
+    Array.from(document.querySelectorAll(selectors.join(","))).forEach((el) => {
+      if (el) {
+        if (isChecked) {
+          el.setAttribute("inert", true);
+        } else {
+          el.removeAttribute("inert");
+        }
+      }
+    });
+  });
+});
