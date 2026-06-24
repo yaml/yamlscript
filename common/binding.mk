fc1caf6781a7a0d6a5e618883f6a3ad12b002809
Ingy döt Net <ingy@ingy.net>
2026-06-24 11:55:11 -0400

    ...

--- common/binding.mk
+++ common/binding.mk
@@ -3,7 +3,11 @@ include $(COMMON)/vars-libys.mk
 test:: $(LIBYS-SO-FQNP)
 
 $(LIBYS-SO-FQNP):
+ifdef YS_RELEASE_USE_INSTALLED_LIBYS
+	test -f $@
+else
 	$(MAKE) -C $(ROOT)/libys build
+endif
 
 build-doc:: $(YS)
 
