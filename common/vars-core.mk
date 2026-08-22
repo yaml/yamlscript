d434ca462f51afaa3d215d40eadfcc874fb1dd47
Ingy döt Net <ingy@ingy.net>
2026-08-22 10:26:57 -0400

    build: Track Clojure common sources
    Include both .clj and .cljc files in core build dependencies.
    
    Ensure shared runtime edits trigger core and native CLI rebuilds.

--- common/vars-core.mk
+++ common/vars-core.mk
@@ -1,7 +1,8 @@
 CORE-DIR := $(ROOT)/core
 CORE-DIR-MAKE := $(ROOT-MAKE)/core
 CORE-JAR := $(CORE-DIR-MAKE)/target/core-$(YAMLSCRIPT_VERSION)-standalone.jar
-CORE-DEPS := $(shell find $(CORE-DIR)/src -name '*.clj')
+CORE-DEPS := $(shell find $(CORE-DIR)/src \
+  \( -name '*.clj' -o -name '*.cljc' \))
 CORE-DEPS := $(CORE-DEPS:$(ROOT)/%=$(ROOT-MAKE)/%)
 
 CORE-INSTALLED := \
